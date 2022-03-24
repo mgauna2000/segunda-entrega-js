4487a066b221a3ddba624f3d68dedb16eabe7af7
Martin <martingauna2000@gmail.com>
2022-03-24 14:00:40 -0300

    segunda entrega

---
+++ js/index.js
@@ -0,0 +1,94 @@
+let productos = [
+  {
+    nombre: "Shingeki No Kyojin",
+    precio: 550,
+    categoria: "manga",
+    stock: 30,
+  },
+  {
+    nombre: "One Piece",
+    precio: 1300,
+    categoria: "cuadro",
+    stock: 20,
+  },
+  {
+    nombre: "Tokio Ghoul",
+    precio: 1300,
+    categoria: "cuadro",
+    stock: 15,
+  },
+  {
+    nombre: "Jujutsu Kaisen",
+    precio: 3500,
+    categoria: "figura de accion",
+    stock: 5,
+  },
+  {
+    nombre: "Akame Ga Kill",
+    precio: 550,
+    categoria: "manga",
+    stock: 100,
+  },
+  {
+    nombre: "Goblin Slayer",
+    precio: 550,
+    categoria: "manga",
+    stock: 80,
+  },
+];
+
+const mostrarProductos = (productos) => {
+  for (const producto of productos) {
+    alert(
+      `el producto es: ${producto.nombre} y su precio es: ${producto.precio}, categoria del producto: ${producto.categoria}`
+    );
+  }
+};
+// mostrarProductos(productos);
+
+const agregarProducto = (productos) => {
+  let nombreProducto = prompt("Nombre del producto");
+  let precioProducto = parseFloat(prompt("Precio del producto"));
+  let categoriaProducto = prompt("Categoria del producto");
+  let stockProducto = parseFloat(prompt("Stock del producto"));
+
+  productos.push({
+    nombre: nombreProducto,
+    precio: precioProducto,
+    categoria: categoriaProducto,
+    stock: stockProducto,
+  });
+  console.table(productos);
+};
+// agregarProducto(productos);
+
+const filtrarProducto = (productos) => {
+  let precioMinimo = parseFloat(prompt("Escribe el precio minimo que desea"));
+  let precioMaximo = parseFloat(prompt("Escribe el precio maximo que desea"));
+
+  const filtrar = productos.filter(
+    (productos) => productos.precio >= precioMinimo && productos.precio <= precioMaximo
+  );
+  console.log(filtrar);
+};
+// filtrarProducto(productos);
+
+const buscarPorCategorias = (productos) => {
+
+  let elijaCategoria = prompt("Elija una de las categorias: manga, cuadro o figura de accion!")
+
+  const productoCategoria = productos.map((productos) =>{
+    if(elijaCategoria == "manga" && productos.categoria == "manga"){
+        return productos.nombre;
+    }else if(elijaCategoria == "cuadro" && productos.categoria == "cuadro"){
+      return productos.nombre;
+    }else if(elijaCategoria == "figura de accion" && productos.categoria == "figura de accion"){
+      return productos.nombre;
+    }
+    else{
+      return null;
+    }
+})
+console.log(productoCategoria);
+}
+// buscarPorCategorias(productos)
