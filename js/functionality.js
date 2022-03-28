8f7f4d68f797ff8587cb4f2af714cb29fce67fdd
Martin <martingauna2000@gmail.com>
2022-03-28 15:28:17 -0300

    update

--- js/functionality.js
+++ js/functionality.js
@@ -1,5 +1,8 @@
 //variables
-const allContainerCart = document.querySelector('.container-products');
+let allContainerCart = document.querySelector('.container-products');
+let containerBuyCart = document.querySelector('.card-items');
+
+let buyThings = [];
 
 //funciones
 loadEventListeners();
@@ -18,11 +21,32 @@ function addProduct(e) {
 
 function readTheContent(product) {
     const infoProduct = {
+        //tomamos los valores del producto seleccionado
         image: product.querySelector('div img').src,
         title: product.querySelector('.product__title').textContent,
         price: product.querySelector('div span').textContent,
         id: product.querySelector('i').getAttribute('data-id'),
         amount: 1
     }
-    console.log(infoProduct)
+    //hacemos una copia del arreglo previo y concatenamos el prodcuto
+    buyThings = [...buyThings, infoProduct];
+    loadHtml();
+}
+
+function loadHtml() {
+    buyThings.forEach(product => {
+        const {image, title, price, id, amount} = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+        <img src="${image}" alt="">
+                        <div class="item-content">
+                            <h5>${title}</h5>
+                            <h5 class="cart-price">${price}$</h5>
+                            <h6>Amount: ${amount}</h6>
+                        </div>
+                        <span class="delete-product" data-id="${id}">X</span>
+        `;
+        containerBuyCart.appendChild(row);
+    });
 }
